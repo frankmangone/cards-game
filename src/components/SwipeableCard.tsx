cecda7960865c5ff5dad160c0e9cd7fc4a86106c
Franco Mangone <francomangone18@gmail.com>
2022-01-07 19:15:29 -0300

    Slightly alters the width of the cards. Placeholder solution

--- src/components/SwipeableCard.tsx
+++ src/components/SwipeableCard.tsx
@@ -3,12 +3,13 @@ import styled from 'styled-components/native'
 import useSwipeableCard from '@hooks/useSwipeableCard'
 import type { VFC } from 'react'
 
+// TODO: Determine width by using viewport width
 const Wrapper = styled(Animated.View)`
   background-color: white;
   border: 1px solid black;
   border-radius: 10px;
   padding: 40px;
-  width: 200px;
+  width: 300px;
 `
 
 const Text = styled.Text`
