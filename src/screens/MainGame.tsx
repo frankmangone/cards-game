5b05946db446033712b0a5213226b827fafc7046
Franco Mangone <francomangone18@gmail.com>
2022-01-07 18:53:58 -0300

    Adds a ScreenWrapper for positioning

--- src/screens/MainGame.tsx
+++ src/screens/MainGame.tsx
@@ -1,11 +1,20 @@
+import styled from 'styled-components/native'
 import MainLayout from '@components/MainLayout'
 import SwipeableCard from '@components/SwipeableCard'
 import type { VFC } from 'react'
 
+const ScreenWrapper = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`
+
 const MainGame: VFC = () => {
   return (
     <MainLayout>
-      <SwipeableCard />
+      <ScreenWrapper>
+        <SwipeableCard />
+      </ScreenWrapper>
     </MainLayout>
   )
 }
