74d28b5574530c36c2e99b8cde7f06d83e2f24ce
Franco Mangone <francomangone18@gmail.com>
2022-01-07 19:07:58 -0300

    Adds path alias for hooks folder

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = function (api) {
               root: ['./src'],
               alias: {
                 '@components': './src/components',
+                '@hooks': './src/components',
                 '@screens': './src/screens',
               },
             },
