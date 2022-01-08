2b55624f652a0535e8b83aefdc0af5bfb1137418
Franco Mangone <francomangone18@gmail.com>
2022-01-08 11:26:19 -0300

    Installs lit-xml for syntax highlight

--- src/screens/MainGame/GameGuessIcon.tsx
+++ src/screens/MainGame/GameGuessIcon.tsx
@@ -1,5 +1,6 @@
 import { SvgXml } from 'react-native-svg'
 import { useTheme } from '@contexts/Theme'
+import { xml } from 'lit-xml'
 
 interface IconProps {
   size?: number
@@ -12,15 +13,15 @@ const GameGuessIcon: React.VFC<IconProps> = (props) => {
   const green = getColor('green')
   const white = getColor('white')
 
-  const xml = `
+  const svg = String(xml`
     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 150 150" style="enable-background:new 0 0 150 150;" xml:space="preserve">
       <circle fill="${green}" cx="75" cy="75" r="69.8"/>
       <polyline stroke="${white}" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="40.78,81.52 61.98,102.73 108.04,56.67 "/>
     </svg>
-  `
+  `)
 
-  return <SvgXml xml={xml} width={size} height={size} />
+  return <SvgXml xml={svg} width={size} height={size} />
 }
 
 export default GameGuessIcon
--- src/screens/MainGame/GamePassIcon.tsx
+++ src/screens/MainGame/GamePassIcon.tsx
@@ -1,5 +1,6 @@
 import { SvgXml } from 'react-native-svg'
 import { useTheme } from '@contexts/Theme'
+import { xml } from 'lit-xml'
 
 interface IconProps {
   size?: number
@@ -12,16 +13,16 @@ const GamePassIcon: React.VFC<IconProps> = (props) => {
   const red = getColor('red')
   const white = getColor('white')
 
-  const xml = `
+  const svg = String(xml`
     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 150 150" style="enable-background:new 0 0 150 150;" xml:space="preserve">
       <circle fill="${red}" cx="75" cy="75" r="69.8"/>
       <line stroke="${white}" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="49.65" y1="48.15" x2="100.35" y2="98.85"/>
       <line stroke="${white}" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="100.35" y1="48.15" x2="49.65" y2="98.85"/>
     </svg>
-  `
+  `)
 
-  return <SvgXml xml={xml} width={size} height={size} />
+  return <SvgXml xml={svg} width={size} height={size} />
 }
 
 export default GamePassIcon
