3ab47d5775898d21f4638adfa04724993e87b6e4
Franco Mangone <francomangone18@gmail.com>
2022-01-08 11:17:56 -0300

    Pulls colors from context

--- src/screens/MainGame/GameGuessIcon.tsx
+++ src/screens/MainGame/GameGuessIcon.tsx
@@ -1,11 +1,17 @@
 import { SvgXml } from 'react-native-svg'
+import { useTheme } from '@contexts/Theme'
 
 const GameGuessIcon: React.VFC = () => {
+  const { getColor } = useTheme()
+
+  const green = getColor('green')
+  const white = getColor('white')
+
   const xml = `
     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 150 150" style="enable-background:new 0 0 150 150;" xml:space="preserve">
-      <circle fill="#476427" cx="75" cy="75" r="69.8"/>
-      <polyline stroke="#FFFFFF" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="40.78,81.52 61.98,102.73 108.04,56.67 "/>
+      <circle fill="${green}" cx="75" cy="75" r="69.8"/>
+      <polyline stroke="${white}" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="40.78,81.52 61.98,102.73 108.04,56.67 "/>
     </svg>
   `
 
--- src/screens/MainGame/GamePassIcon.tsx
+++ src/screens/MainGame/GamePassIcon.tsx
@@ -1,15 +1,18 @@
 import { SvgXml } from 'react-native-svg'
+import { useTheme } from '@contexts/Theme'
 
 const GamePassIcon: React.VFC = () => {
+  const { getColor } = useTheme()
+
+  const red = getColor('red')
+  const white = getColor('white')
+
   const xml = `
     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 150 150" style="enable-background:new 0 0 150 150;" xml:space="preserve">
-      <style type="text/css">
-        .st1{stroke-linejoin:round;stroke-miterlimit:10;}
-      </style>
-      <circle fill="#AB2C17" cx="75" cy="75" r="69.8"/>
-      <line stroke="#FFFFFF" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="49.65" y1="48.15" x2="100.35" y2="98.85"/>
-      <line stroke="#FFFFFF" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="100.35" y1="48.15" x2="49.65" y2="98.85"/>
+      <circle fill="${red}" cx="75" cy="75" r="69.8"/>
+      <line stroke="${white}" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="49.65" y1="48.15" x2="100.35" y2="98.85"/>
+      <line stroke="${white}" stroke-width="16" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="100.35" y1="48.15" x2="49.65" y2="98.85"/>
     </svg>
   `
 
