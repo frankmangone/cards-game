baff012fa10782c7a44dc4061a7af42986876583
Franco Mangone <francomangone18@gmail.com>
2022-01-16 16:32:17 -0300

    Adds check icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BaseIcon from './BaseIcon'
 
 /* Icon raw xmls (as functions with param color) */
+import check from '@lib/icons/check'
 import remainingCards from '@lib/icons/remainingCards'
 import timer from '@lib/icons/timer'
 
@@ -15,6 +16,10 @@ interface IconProps {
  *
  * Remember to add prop aspectRatio (width / height) if it's different than 1
  *  */
+
+export const CheckIcon: React.VFC<IconProps> = (props): JSX.Element => (
+  <BaseIcon xml={check} {...props} />
+)
 
 export const RemainingCardsIcon: React.VFC<IconProps> = (props): JSX.Element => (
   <BaseIcon xml={remainingCards} {...props} />
